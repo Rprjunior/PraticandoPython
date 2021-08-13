0300d836d923e8c26752db1931071db79f231bd4
Júnior Pereira <junior.rprj22@gmail.com>
2021-08-13 14:51:17 -0300

    Exercicio 71 CURSO EM VIDEO.

---
+++ 071 - Caixa Python 1.0.py
@@ -0,0 +1,25 @@
+saque = int(input('Qual o valor do saque. R$ '))
+cedula = 100
+total_cedulas = 0
+while True:
+    if saque >= cedula:
+        saque = saque - cedula
+        total_cedulas = total_cedulas + 1
+    else:
+        if total_cedulas > 0:
+            print(f'{total_cedulas} cédula(s) de R$ {cedula}.')
+        if cedula == 100:
+            cedula = 50
+        elif cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 5
+        elif cedula == 5:
+            cedula = 2
+        total_cedulas = 0
+        if saque == 0 or saque == 1:
+            break
+print('{:^30}'.format('OPERAÇÃO FINALIZADA'))
+print('{:^30}'.format('VOLTE SEMPRE'))
