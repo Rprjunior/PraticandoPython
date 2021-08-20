3c6f3ed9e7aaa62332de727cfb3260b69c3e8c44
Júnior <junior.rprj22@gmail.com>
2021-08-20 18:47:35 -0300

    Exercicio 16 - CURSO EM VÍDEO.

--- 016 - Número Inteiro.py
+++ 016 - Número Inteiro.py
@@ -1,5 +1,5 @@
 '''016 - NÚMERO INTEIRO.
-PROGRAMA QUE LEIA UM VALOR REAL E MOSTRE APENAS SUA PARTE INTEIRA.'''
+PROGRAMA QUE LEIA UM VALOR REAL E MOSTRE SUA PARTE INTEIRA.'''
 
 num = float(input('Digite um valor: '))
 print('O número digitado foi {} e sua parte inteira é: {}'.format(num, int(num)))
