0cc2cefa300be58a5e62935b5fe6e39be851530c
Júnior <junior.rprj22@gmail.com>
2021-08-20 17:50:35 -0300

    Exercicio 03 - CURSO EM VÍDEO.

---
+++ 003 - 1° Soma.py
@@ -0,0 +1,8 @@
+'''003 - 1° SOMA.
+PROGRAMA QUE LEIA DOIS VALORES E FAÇA A SOMA ENTRE ELES.'''
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1+n2
+print('A soma entre {} + {} é: {}'.format(n1, n2, s))
+print('A soma entre', n1, '+', n2, 'é:', s)
