97ad33a008495304590a459bc44eefab90b79c9a
Júnior <junior.rprj22@gmail.com>
2021-08-20 18:12:30 -0300

    Exercicio 06 - CURSO EM VÍDEO.

--- 006 - Dobro, Triplo, Raiz.py
+++ 006 - Dobro, Triplo, Raiz.py
@@ -14,12 +14,3 @@ print('O dobro de {} é: {}'.format(n, d))
 print('O triplo de {} é: {}'.format(n, t))
 print('A raíz de {} é: {:.2f}'.format(n, r))
 print('A raiz cúbica de {} é: {:.2f}'.format(n, rc))
-
-'''010 - CONVERSOR DE DÓLAR.
-PROGRAMA QUE LEIA UM VALOR E FAÇA A COTAÇÃO EM DÓLAR.'''
-
-
-R = float(input('Digite um valor R$: '))
-D = R / 5.06
-print('Esse valor R${}, convertido para US$ fica: US${:.1f}'.format(R,D))
-print('Você tem R${}, se trocar por US$ ficará: US${:.2f}'.format(R, (R/5.06)))
