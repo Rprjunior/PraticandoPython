c281386878f5fd130fe88efe5b8733d18a8da429
Júnior <junior.rprj22@gmail.com>
2021-08-20 17:32:12 -0300

    Exercicio 01 - CURSO EM VÍDEO.

---
+++ 001 - Olá Mundo.py
@@ -0,0 +1,5 @@
+'''PRIMEIRO PROGRAMA EM PYTHON. VAMOS SEGUINDO A TRADIÇÃO PARA NOS TRA\ER SORTE.'''
+
+print("Olá Mundo")
+nome = input("Qual o seu nome? ")
+print("Seja bem vindo, {}. É um prazer te conhecer!".format(nome))
