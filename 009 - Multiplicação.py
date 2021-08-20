0684047b8c4b20ae99810a24cdeb863932f9bd67
Júnior <junior.rprj22@gmail.com>
2021-08-20 18:10:02 -0300

    Exercicio 09 - CURSO EM VÍDEO.

---
+++ 009 - Multiplicação.py
@@ -0,0 +1,18 @@
+'''009 - MULTIPLICAÇÃO.
+PROGRAMA QUE LEIA UM VALOR E MULTIPLIQUE ATÉ 10.'''
+
+num = int(input('Digite um valor para fazermos sua tabuada: '))
+print('-'*15)
+print('MULTIPLICAÇÃO')
+print('-'*15)
+print('{} * {:2} = {}'.format(num, 1, (num*1)))
+print('{} * {:2} = {}'.format(num, 2, (num*2)))
+print('{} * {:2} = {}'.format(num, 3, (num*3)))
+print('{} * {:2} = {}'.format(num, 4, (num*4)))
+print('{} * {:2} = {}'.format(num, 5, (num*5)))
+print('{} * {:2} = {}'.format(num, 6, (num*6)))
+print('{} * {:2} = {}'.format(num, 7, (num*7)))
+print('{} * {:2} = {}'.format(num, 8, (num*8)))
+print('{} * {:2} = {}'.format(num, 9, (num*9)))
+print('{} * {:2} = {}'.format(num, 10, (num*10)))
+print('-'*15)
