782194396a94c303e9da99811cc1b5d27de138c1
Júnior <junior.rprj22@gmail.com>
2021-08-20 17:50:16 -0300

    Exercicio 02 - CURSO EM VÍDEO.

---
+++ 002 - 1° Nome.py
@@ -0,0 +1,6 @@
+'''002 - 1° NOME.
+ PROGRAMA QUE LEIA UM NOME DE UMA PESSOA E MOSTRE NA TELA.'''
+
+nome = input('Digite seu nome: ')
+print('Muito prazer em te conhecer,', nome, '.')
+print('Prazer em te conhecer, {}.'.format(nome))
