c54b52cf6c322b8bf7b1e4e20cc92da9190f5d1c
Júnior <junior.rprj22@gmail.com>
2021-10-20 15:50:07 -0300

    CURSO EM VÍDEO - EXERCICIO 46

---
+++ 046 - Contagem Regressiva.py
@@ -0,0 +1,10 @@
+'''046 - CONTAGEM REGRESSIVA
+PROGRAMA QUE MOSTRE NA TELA UM ACONTAGEM REGRESSIVA.'''
+
+from time import sleep
+
+for contagem in range(5, -1, -1):
+    print(contagem)
+    sleep(1)
+
+print('KAMBOOM! BOOM! TU! DU! DU! PÁ! IHULL')
